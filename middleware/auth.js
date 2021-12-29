23054a6eeb613e6059054668169955218cd1c6e4
Jose Nortes <nooors@gmail.com>
2021-12-29 11:48:35 +0100

    store set in modules option

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,9 @@ import Vuex from "vuex";
 
 export default function ({ store, redirect }) {
   // If the user is not authenticated
-  if (!store.state.authenticated) {
+  console.log("Middleware");
+  console.log(store.state);
+  if (!store.state) {
     console.log(store.state);
     return redirect("/Login");
   }
