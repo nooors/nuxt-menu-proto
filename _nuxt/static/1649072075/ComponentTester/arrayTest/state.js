a2cb27b26509fc90d5700fdc03203bc355c3df91
Jose Nortes <nooors@gmail.com>
2022-04-04 13:34:54 +0200

    Updates

---
+++ _nuxt/static/1649072075/ComponentTester/arrayTest/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-menu-proto\u002F_nuxt\u002Fstatic\u002F1649072075",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002FComponentTester\u002FarrayTest",config:{_app:{basePath:"\u002Fnuxt-menu-proto\u002F",assetsPath:"\u002Fnuxt-menu-proto\u002F_nuxt\u002F",cdnURL:a}}}}(null));
